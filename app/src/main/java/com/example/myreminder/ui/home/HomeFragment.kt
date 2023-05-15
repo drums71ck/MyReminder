7dc95f60c18955d135675ea1891c2c9e961d4866
drums71ck <theyayo500@gmail.com>
2023-05-15 17:21:49 +0200

    creas notas y tal epico

--- app/src/main/java/com/example/myreminder/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/myreminder/ui/home/HomeFragment.kt
@@ -3,11 +3,14 @@ package com.example.myreminder.ui.home
 import android.graphics.Color
 import android.media.Image
 import android.os.Bundle
+import android.view.Gravity
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.LinearLayout
 import android.widget.PopupWindow
 import android.widget.RelativeLayout
 
@@ -24,8 +27,11 @@ class HomeFragment : Fragment() {
     lateinit var btnColorPicker : ImageButton
     lateinit var btnTextColor: ImageButton
     lateinit var btnClock: ImageButton
+    lateinit var btnAddPostIt : ImageButton
     lateinit var txtTittle: EditText
     lateinit var txtContent: EditText
+    lateinit var containerPostIt : LinearLayout
+    private var postItCount = 0
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -41,15 +47,32 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
         val postItLayout = root.findViewById<RelativeLayout>(R.id.postItLayout)
+
+        // Definimos variables
         btnColorPicker = root.findViewById(R.id.colorButton)
         txtTittle = root.findViewById(R.id.titleEditText)
         txtContent = root.findViewById(R.id.contentEditText)
         btnTextColor = root.findViewById(R.id.textColorButton)
+        btnAddPostIt = root.findViewById(R.id.add_fragment_button)
+        containerPostIt = root.findViewById(R.id.containerPostIt)
+
+        txtTittle.setOnKeyListener { _, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+                txtContent.requestFocus()
+                return@setOnKeyListener true
+            }
+            return@setOnKeyListener false
+        }
+
+        // Controladores de click de los botones
         btnColorPicker.setOnClickListener(){
             randomColor(postItLayout)
         }
         btnTextColor.setOnClickListener(){
             randomTextColor()
+        }
+        btnAddPostIt.setOnClickListener(){
+            addPostItLayout()
         }
         return root
     }
@@ -74,6 +97,56 @@ class HomeFragment : Fragment() {
         txtTittle.setTextColor(colorT)
     }
 
+    /**
+     * Esta Función lo que hace en pocas palabras es clonar los atributos del post_it
+     * agregandolo nueva id
+     * @sample so
+     */
+    private fun addPostItLayout() {
+        postItCount++
+        val inflater = LayoutInflater.from(requireContext())
+        val postItView = inflater.inflate(R.layout.post_it, containerPostIt, false)
+        val uniqueID = View.generateViewId()
+        postItView.id = uniqueID
+
+        // Obtenemos el layout y tamaño del post IT
+        val postItLayout = requireView().findViewById<RelativeLayout>(R.id.postItLayout)
+        val postItWidth = postItLayout.width
+        val postItHeight = postItLayout.height
+
+
+        // Le decimos que al crear mantenga un espacio y se situe en el centro
+        val layoutParams = LinearLayout.LayoutParams(postItWidth, postItHeight)
+        layoutParams.topMargin = resources.getDimensionPixelSize(R.dimen.post_it_margin_top)
+        layoutParams.gravity = Gravity.CENTER_HORIZONTAL
+
+        postItView.layoutParams = layoutParams
+
+        // Cargamos los botones creados de cada clone
+        val colorButton = postItView.findViewById<ImageButton>(R.id.colorButton)
+        val textColorButton = postItView.findViewById<ImageButton>(R.id.textColorButton)
+        val addButton = postItView.findViewById<ImageButton>(R.id.add_fragment_button)
+        val txtTittleClone = postItView.findViewById<EditText>(R.id.titleEditText)
+        val txtContentClone = postItView.findViewById<EditText>(R.id.contentEditText)
+
+        colorButton.setOnClickListener {
+            randomColor(postItView as RelativeLayout)
+        }
+        textColorButton.setOnClickListener {
+
+                val rnd = Random()
+                val colorT: Int = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
+
+            txtTittleClone.setTextColor(colorT)
+            txtContentClone.setTextColor(colorT)
+
+        }
+        addButton.setOnClickListener {
+            addPostItLayout()
+        }
+
+        containerPostIt.addView(postItView)
+    }
 
 
 }
