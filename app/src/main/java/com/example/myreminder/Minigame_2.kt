f7e7ce9284d4fb7376a45404d93c8dbca88ef6a0
drums71ck <theyayo500@gmail.com>
2023-05-26 17:49:50 +0200

    mas bubbles

--- app/src/main/java/com/example/myreminder/Minigame_2.kt
+++ app/src/main/java/com/example/myreminder/Minigame_2.kt
@@ -3,6 +3,7 @@ package com.example.myreminder
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Toast
@@ -15,7 +16,11 @@ class Minigame_2 : AppCompatActivity() {
     private  val buttonsIds = arrayOf(
         R.id.boton00, R.id.boton01, R.id.boton02, R.id.boton03,
         R.id.boton04, R.id.boton05, R.id.boton06, R.id.boton07,
-        R.id.boton08, R.id.boton09, R.id.boton10, R.id.boton11
+        R.id.boton08, R.id.boton09, R.id.boton10, R.id.boton11,
+        R.id.boton12, R.id.boton13, R.id.boton14, R.id.boton15,
+        R.id.boton15, R.id.boton16, R.id.boton17, R.id.boton18,
+        R.id.boton19, R.id.boton20, R.id.boton21, R.id.boton22,
+        R.id.boton23,
     )
     private val soundIds = arrayOf(
         R.raw.bubble1, R.raw.bubble2, R.raw.bubble3
@@ -40,7 +45,8 @@ class Minigame_2 : AppCompatActivity() {
     }
 
     /**
-     * TODO
+     * Esta función estara encarga de controlar
+     * las burbujas presionadas y darles un sonido
      *
      * @param button
      */
@@ -49,10 +55,11 @@ class Minigame_2 : AppCompatActivity() {
         button.isClickable = false
         bubblesCount--
 
-        // Reproducir los sonidos de burbujas aleatoriamente
-        val randomSoundId = if(Random().nextInt(100)<1){
+        // Reproducir sonido de burbuja aleatorio
+        val randomSoundId = if (Random().nextInt(100) < 2) {
+            // Probabilidad del 2% de reproducir el sonido meow
             R.raw.meow
-        }else{
+        } else {
             soundIds.random()
         }
 
@@ -64,17 +71,28 @@ class Minigame_2 : AppCompatActivity() {
         try {
             mediaPlayer.setDataSource(resources.openRawResourceFd(randomSoundId))
             mediaPlayer.prepare()
+
+            if (randomSoundId == R.raw.meow) {
+                // Bajar el volumen del sonido "meow" a la mitad
+                mediaPlayer.setVolume(0.3f, 0.3f)
+            }
+
             mediaPlayer.start()
         } catch (e: IOException) {
             e.printStackTrace()
             // Manejar la excepción de manera adecuada
         }
-
-        if (bubblesCount == 0) {
+        if (bubblesCount == 1) {
             restartGame()
         }
     }
 
+    /**
+     * Lo que hace esta función es sencillo
+     * reiniciara el juego voliendo cargar
+     * la imagen de la burbuja
+     *
+     */
     private fun restartGame() {
         for (buttonId in buttonsIds){
             val button: ImageButton = findViewById(buttonId)
